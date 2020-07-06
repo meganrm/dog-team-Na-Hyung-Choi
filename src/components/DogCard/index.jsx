e3653b07da2b90ee16052ed2f4609abd7459b807
Na Hyung Choi <schoinh@gmail.com>
2020-07-06 16:40:31 -0700

    Display tags in card title

--- src/components/DogCard/index.jsx
+++ src/components/DogCard/index.jsx
@@ -5,10 +5,11 @@ import { Card, Col } from 'antd';
 const DogPair = ({
     image,
     name,
+    tags
 }) => (
     <Col span={8}>
        <Card
-            title={name}
+            title={`${name} - ${tags.join(', ')}`}
             cover={<img src={`${process.env.PUBLIC_URL}/images/${image}`} alt={`husky named ${name}`}/>}
        >
        </Card>
